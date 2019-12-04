2a5fcee42df364aebb7d5835051cbec836eac6ea
cthorrez <thorrez@umich.edu>
2019-12-04 13:05:11 -0500

    added queue diversity

--- launch_jobs.py
+++ launch_jobs.py
@@ -11,11 +11,13 @@ def main():
     os.mkdir(path)
     shutil.copy('config.json', path)
 
-    
-    for i in range(args['n_trials']):
+    queues = 24*['1080ti-short'] + 12*['1080ti-long'] + 24*['titanx-short']
+
+    queues = 3*['1080ti-short'] + 3*['1080ti-long'] + 3*['titanx-short'] + 3*['titanx-short'] + 3*['m40-short'] + 3*['m40-short']
+    for i, q in zip(np.arange(args['n_trials']), queues):
         # run trails with different seeds
         fname = path + '/results_' + str(i)
-        command = 'sbatch --partition=1080ti-short --gres=gpu:1 --job-name=deepset --output={} --ntasks-per-node=1 --mem=25G run.sh {}'.format(fname, i)
+        command = 'sbatch --partition={} --gres=gpu:1 --job-name=deepset --output={} --ntasks-per-node=1 --mem=25G run.sh {}'.format(q, fname, i)
         os.system(command)
         time.sleep(1)
 
